cd4e14cd596cb510f83bf1a27352ae02cc8187b8
Lei <yuni0425@gmail.com>
2025-10-31 10:20:24 +0800

    [feat] 旅程頁面功能 - 架 DB plan table - API: 查詢旅程列表、新增整趟旅程

--- backend/src/routes/plans/plans.routes.ts
+++ backend/src/routes/plans/plans.routes.ts
@@ -2,6 +2,8 @@ import express from "express";
 import type { Request, Response, NextFunction } from "express";
 import { prisma } from "../../utils/prisma-only.js"
 import { authMiddleware } from "../../middleware/authMiddleware.js";
+import { success } from "zod";
+import { serializeBigInt } from "../../utils/serializeBigInt.js"
 
 const router = express.Router();
 
@@ -14,5 +16,71 @@ const router = express.Router();
 // | DELETE | /api/plans/:planId | 刪除旅程 |
 
 // #endregion
+
+// | GET | /api/plans | 讀取所有旅程 |
+router.get("/", async (req: Request, res: Response) => {
+    const userId = 1;
+
+    if (!userId) return res.status(404).json({ message: "沒有提供User ID" })
+
+    try {
+      const plans = await prisma.plan.findMany({
+        where: {
+            userId: userId,
+        },
+        orderBy: {
+            startDate: "desc",
+        }
+      })
+      res.json(serializeBigInt(plans));
+    } catch (err) {
+      res.status(500).json({ error: (err as Error).message});
+    }
+})
+
+// | POST | /api/plans | 新增旅程 |
+router.post("/", async (req: Request, res: Response) => {
+  const userId = 1; //之後改為從 JWT 取 userID
+
+  if (!userId) return res.status(404).json({ message: "沒有提供User ID" })
+
+  try {
+    const {
+      title,
+      destination,
+      startDate,
+      startTimezone,
+      endDate,
+      endTimezone,
+      note,
+      coverImage,
+    } = req.body;
+
+    const newPlan = await prisma.plan.create({
+      data: {
+        userId,
+        title,
+        destination, //選填
+        startDate,
+        startTimezone,
+        endDate,
+        endTimezone,
+        note,  //選填
+        coverImage, //選填
+      },
+    });
+
+    res.status(201).json({ 
+      success: true,
+      message: "旅程新增成功",
+    })
+  } catch (err) {
+    res.status(500).json({ 
+      success: false,
+      message: "旅程新增失敗，",
+      error: (err as Error).message
+    });
+  }
+})
 
 export default router;
