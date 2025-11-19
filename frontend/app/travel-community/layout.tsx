2d5067747dbda16aa5aa0b35fda1fb5bec71455e
baron <zichenlin0515@gmail.com>
2025-11-19 11:28:12 +0800

    改寬度

--- frontend/app/travel-community/layout.tsx
+++ frontend/app/travel-community/layout.tsx
@@ -1,5 +1,5 @@
 // app/travel-community/layout.tsx
-import "../../styles/globals.css";
+import '../../styles/globals.css';
 
 export default function TravelLayout({
   children,
@@ -7,7 +7,7 @@ export default function TravelLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="mx-auto w-full max-w-[1312px] px-4 sm:px-6 lg:px-[64px] py-6">
+    <section className="mx-auto w-full max-w-[1440px] px-4 sm:px-6 py-6">
       {children}
     </section>
   );
